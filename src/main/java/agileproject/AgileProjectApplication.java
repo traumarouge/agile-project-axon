3d6781110a9a587e43a7b91939b07d54f65d2dbb
traumarouge <traumarouge@gmail.com>
2017-12-28 16:48:01 +0100

    Add initial project structure (Spring Boot + Axon)

---
+++ src/main/java/agileproject/AgileProjectApplication.java
@@ -0,0 +1,12 @@
+package agileproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileProjectApplication.class, args);
+	}
+}
