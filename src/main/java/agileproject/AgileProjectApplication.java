73defdaa15a4b18a1da9c2f6334c4cb1f41a43f3
traumarouge <traumarouge@gmail.com>
2018-01-04 15:13:35 +0100

    Configure EventStorageEngine
    In order to use event sourcing, an event storage engine must be
    configured. For the moment, an event storage engine that stores
    events in memory will suffice.

--- src/main/java/agileproject/AgileProjectApplication.java
+++ src/main/java/agileproject/AgileProjectApplication.java
@@ -1,12 +1,26 @@
 package agileproject;
 
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class AgileProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgileProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(AgileProjectApplication.class, args);
+    }
+
+
+    @Bean
+    public EventStorageEngine eventStorageEngine() {
+
+        return new InMemoryEventStorageEngine();
+    }
 }
