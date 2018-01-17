491cfb941e7b94f0ac8ce600ddd396350a49ff98
traumarouge <traumarouge@gmail.com>
2018-01-17 10:48:11 +0100

    Persist events to embedded (H2) database using JPA

--- src/main/java/agileproject/AgileProjectApplication.java
+++ src/main/java/agileproject/AgileProjectApplication.java
@@ -1,12 +1,7 @@
 package agileproject;
 
-import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
-import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -15,12 +10,5 @@ public class AgileProjectApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(AgileProjectApplication.class, args);
-    }
-
-
-    @Bean
-    public EventStorageEngine eventStorageEngine() {
-
-        return new InMemoryEventStorageEngine();
     }
 }
